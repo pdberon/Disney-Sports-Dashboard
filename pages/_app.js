38fa80008bc72c6cbb2f18318c1890ef1fdd4f19
pdberon <73356763+pdberon@users.noreply.github.com>
2026-06-23 15:31:20 -0300

    Initialize _app.js with global styles
    Add global styles and main App component for Next.js.

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/globals.css' // Ajusta esta ruta si tu archivo de estilos de Tailwind está en otra ubicación
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
